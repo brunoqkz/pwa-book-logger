ff37bc4a9d9e19a5a8e67e32c66765224c27f80f
Bruno Ribeiro da Silva <brunoqkz@gmail.com>
2025-02-23 23:39:39 -0500

    fix: added files to cache

--- service-worker.js
+++ service-worker.js
@@ -2,15 +2,17 @@ const CACHE_NAME = "book-logger-cache-v1";
 const FILES_TO_CACHE = [
   "/pwa-book-logger/",
   "/pwa-book-logger/assets/css/style.css",
-  "/pwa-book-logger/assets/html/index.html",
   "/pwa-book-logger/assets/html/books.html",
+  "/pwa-book-logger/assets/html/index.html",
   "/pwa-book-logger/assets/html/offline.html",
   "/pwa-book-logger/assets/icons/favicon.ico",
+  "/pwa-book-logger/assets/icons/google-icon.png",
   "/pwa-book-logger/assets/icons/icon-128.png",
   "/pwa-book-logger/assets/icons/icon-512.png",
+  "/pwa-book-logger/assets/js/ai.js",
+  "/pwa-book-logger/assets/js/books.js",
   "/pwa-book-logger/assets/js/firebase.js",
   "/pwa-book-logger/assets/js/signIn.js",
-  "/pwa-book-logger/assets/js/books.js",
   "/pwa-book-logger/manifest.json",
 ];
 
@@ -27,4 +29,3 @@ self.addEventListener("fetch", (event) => {
       .then((response) => response || fetch(event.request)),
   );
 });
-
