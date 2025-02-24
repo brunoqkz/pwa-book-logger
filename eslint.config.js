b8025cbde809612b666e718cefad3f75a9c7a4ea
Bruno Ribeiro da Silva <brunoqkz@gmail.com>
2025-02-23 23:33:35 -0500

    refactor: simplified files based on previous project

--- eslint.config.js
+++ eslint.config.js
@@ -5,28 +5,6 @@ export default [
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
-      "no-console": ["warn", { allow: ["error", "warn", "info"] }],
-      "prefer-const": "warn",
-      "no-var": "warn",
-      eqeqeq: ["warn", "always"],
-      curly: ["warn", "all"],
-      indent: ["warn", 2],
-      quotes: ["warn", "double"],
-      semi: ["warn", "always"],
-      "space-before-blocks": "warn",
-      "space-before-function-paren": [
-        "warn",
-        {
-          anonymous: "always",
-          named: "never",
-          asyncArrow: "always",
-        },
-      ],
-      "space-in-parens": ["warn", "never"],
-      "space-infix-ops": "warn",
-      "keyword-spacing": "warn",
-      "comma-spacing": ["warn", { before: false, after: true }],
-      "no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 0 }],
     },
     files: ["*.js"],
   },
@@ -39,6 +17,6 @@ export default [
     },
   },
   {
-    ignores: ["dist/", "node_modules/", "docs/"],
+    ignores: ["dist/", "node_modules/"],
   },
 ];
